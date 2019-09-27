5bf04243253f0e98685f9815ad3039dc12046d46
young <974166167@qq.com>
2019-09-27 09:45:31 +0800

    11

---
+++ src/main/java/com/mycompany/Disruptor/bhz/generate2/Handler3.java
@@ -0,0 +1,12 @@
+package com.mycompany.Disruptor.bhz.generate2;
+
+
+import com.lmax.disruptor.EventHandler;
+import com.mycompany.Disruptor.bhz.generate1.Trade;
+
+public class Handler3 implements EventHandler<Trade> {
+    @Override  
+    public void onEvent(Trade event, long sequence,  boolean endOfBatch) throws Exception {  
+    	System.out.println("handler3: name: " + event.getName() + " , price: " + event.getPrice() + ";  instance: " + event.toString());
+    }  
+}
